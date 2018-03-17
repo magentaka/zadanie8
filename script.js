6bd450951efbee7ff37bf6ced04571e72ed0ded1
marzena kaniak <marzena.kaniak@gmail.com>
2018-03-18 00:47:07 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+  this is my block comment;
+*/
+var name = prompt('Enter your name');
+alert('Hello, ' + name); //this is my linear comment
+console.log('Hello, ' + name); //this is my second linear comment
